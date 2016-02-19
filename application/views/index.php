786376b4a2e593771a752318024ac70078af7cf3
Barclay Iversen <barclayiversen@gmail.com>
2016-02-18 17:47:35 -0800

    initial commit

---
+++ application/views/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <meta charset="utf-8">
+        <style>
+            #manage{
+                height: 300px;
+                width: 300px;
+                display: inline-block;
+
+                vertical-align: top;
+                margin-left: 60px;
+                margin-right: 50px;
+            }
+            #message{
+                height: 300px;
+                width: 300px;
+                display: inline-block;
+
+                margin-right: 50px;
+            }
+            #information{
+                height: 300px;
+                width: 300px;
+                display: inline-block;
+                
+            }
+            .bold{
+                font-weight: bold;
+            }
+        </style>
+        <title>welcome page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h3>Welcome to the Test</h3>
+                <p>We're going to build a cool application using MVC. Yadda Yadda Yadda</p>
+                <p>YAdda Yadda</p>
+                <button class='btn btn-primary'>Start</button>
+            </div>
+            <div id="manage">
+                <p class="bold">Manage Users</p>
+                <p>using this application you'll learn how to add blah blah blah</p>
+            </div>
+            <div id="message">
+                <p class="bold">Leave messages</p>
+                <p>using this application you'll learn how to add blah blah blah</p>
+            </div>
+            <div id="information">
+                <p class="bold">Edit user info</p>
+                <p>using this application you'll learn how to add blah blah blah</p>
+            </div>
+        </div>
+    </body>
+</html>
